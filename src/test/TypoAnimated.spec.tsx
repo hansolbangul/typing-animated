ded28d2190f0066cebe32a7cf03602cc912e1804
hansolbangul <ruaenddl981028@naver.com>
2024-10-25 17:46:38 +0900

    [Feat] prettier 세팅

--- src/test/TypoAnimated.spec.tsx
+++ src/test/TypoAnimated.spec.tsx
@@ -1,74 +1,78 @@
-import {act, render, screen, waitFor} from '@testing-library/react';
-import {TypoAnimated} from "../components/TypoAnimated";
+import { act, render, screen, waitFor } from '@testing-library/react';
+import { TypoAnimated } from '../components/TypoAnimated';
 
 jest.useFakeTimers();
 
-const ms = 100
-const wait = 500
+const ms = 100;
+const wait = 500;
 
 describe('<TypoAnimated /> 컴포넌트 테스트', () => {
-    test('animates text correctly over time', async () => {
-        render(<p data-testid="typing-text"><TypoAnimated text="Hello" ms={ms} wait={wait}/></p>);
+  test('animates text correctly over time', async () => {
+    render(
+      <p data-testid="typing-text">
+        <TypoAnimated text="Hello" ms={ms} wait={wait} />
+      </p>,
+    );
 
-        const typingElement = screen.getByTestId('typing-text');
+    const typingElement = screen.getByTestId('typing-text');
 
-        expect(typingElement.textContent).toBe('');
+    expect(typingElement.textContent).toBe('');
 
-        act(() => {
-            jest.advanceTimersByTime(ms);
-        });
-        await waitFor(() => expect(typingElement.textContent).toBe('H'));
-
-        act(() => {
-            jest.advanceTimersByTime(ms);
-        });
-        await waitFor(() => expect(typingElement.textContent).toBe('He'));
-
-        act(() => {
-            jest.advanceTimersByTime(ms);
-        });
-        await waitFor(() => expect(typingElement.textContent).toBe('Hel'));
-
-        act(() => {
-            jest.advanceTimersByTime(ms);
-        });
-        await waitFor(() => expect(typingElement.textContent).toBe('Hell'));
-
-        act(() => {
-            jest.advanceTimersByTime(ms);
-        });
-        await waitFor(() => expect(typingElement.textContent).toBe('Hello'));
-
-        act(() => {
-            jest.advanceTimersByTime(wait);
-        });
-        await waitFor(() => expect(typingElement.textContent).toBe('Hello'));
-
-        screen.debug()
-
-        act(() => {
-            jest.advanceTimersByTime(ms);
-        });
-        await waitFor(() => expect(typingElement.textContent).toBe('Hell'));
-
-        act(() => {
-            jest.advanceTimersByTime(ms);
-        });
-        await waitFor(() => expect(typingElement.textContent).toBe('Hel'));
-
-        act(() => {
-            jest.advanceTimersByTime(ms);
-        });
-        await waitFor(() => expect(typingElement.textContent).toBe('He'));
-
-        act(() => {
-            jest.advanceTimersByTime(ms);
-        });
-        await waitFor(() => expect(typingElement.textContent).toBe('H'));
-
-        act(() => {
-            jest.advanceTimersByTime(ms);
-        });
-        await waitFor(() => expect(typingElement.textContent).toBe(''));
+    act(() => {
+      jest.advanceTimersByTime(ms);
     });
+    await waitFor(() => expect(typingElement.textContent).toBe('H'));
+
+    act(() => {
+      jest.advanceTimersByTime(ms);
+    });
+    await waitFor(() => expect(typingElement.textContent).toBe('He'));
+
+    act(() => {
+      jest.advanceTimersByTime(ms);
+    });
+    await waitFor(() => expect(typingElement.textContent).toBe('Hel'));
+
+    act(() => {
+      jest.advanceTimersByTime(ms);
+    });
+    await waitFor(() => expect(typingElement.textContent).toBe('Hell'));
+
+    act(() => {
+      jest.advanceTimersByTime(ms);
+    });
+    await waitFor(() => expect(typingElement.textContent).toBe('Hello'));
+
+    act(() => {
+      jest.advanceTimersByTime(wait);
+    });
+    await waitFor(() => expect(typingElement.textContent).toBe('Hello'));
+
+    screen.debug();
+
+    act(() => {
+      jest.advanceTimersByTime(ms);
+    });
+    await waitFor(() => expect(typingElement.textContent).toBe('Hell'));
+
+    act(() => {
+      jest.advanceTimersByTime(ms);
+    });
+    await waitFor(() => expect(typingElement.textContent).toBe('Hel'));
+
+    act(() => {
+      jest.advanceTimersByTime(ms);
+    });
+    await waitFor(() => expect(typingElement.textContent).toBe('He'));
+
+    act(() => {
+      jest.advanceTimersByTime(ms);
+    });
+    await waitFor(() => expect(typingElement.textContent).toBe('H'));
+
+    act(() => {
+      jest.advanceTimersByTime(ms);
+    });
+    await waitFor(() => expect(typingElement.textContent).toBe(''));
+  });
 });
