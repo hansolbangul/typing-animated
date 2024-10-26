f0441ed84500b154fe87546e729fa9d5f6073393
hansolbangul <ruaenddl981028@naver.com>
2024-10-26 16:06:06 +0900

    [Feat] version 수정 (v.1.0.7)

--- src/components/TypoAnimated.tsx
+++ src/components/TypoAnimated.tsx
@@ -4,7 +4,7 @@ interface Props {
   ms: number;
   wait: number;
   loop?: number;
-  text: string;
+  text: string | string[];
 }
 
 export function TypoAnimated({ text, ms, wait, loop = -1 }: Props) {
@@ -15,18 +15,35 @@ export function TypoAnimated({ text, ms, wait, loop = -1 }: Props) {
 
     const run = function* () {
       while (remainingLoops > 0 || remainingLoops === -1) {
-        for (let i = 0; i <= text.length; i++) {
-          yield text.slice(0, i);
-        }
-
-        yield new Promise((resolve) => setTimeout(resolve, ms));
-
-        if (remainingLoops !== 1) {
-          for (let i = text.length - 1; i >= 0; i--) {
+        if (typeof text === 'string') {
+          for (let i = 0; i <= text.length; i++) {
             yield text.slice(0, i);
           }
 
           yield new Promise((resolve) => setTimeout(resolve, ms));
+
+          if (remainingLoops !== 1) {
+            for (let i = text.length - 1; i >= 0; i--) {
+              yield text.slice(0, i);
+            }
+
+            yield new Promise((resolve) => setTimeout(resolve, ms));
+          }
+        } else {
+          for (const word of text) {
+            for (let i = 0; i <= word.length; i++) {
+              yield word.slice(0, i);
+            }
+
+            yield new Promise((resolve) => setTimeout(resolve, wait));
+
+            if (remainingLoops !== 1 || word !== text[text.length - 1]) {
+              for (let i = word.length - 1; i >= 0; i--) {
+                yield word.slice(0, i);
+              }
+              yield new Promise((resolve) => setTimeout(resolve, ms));
+            }
+          }
         }
 
         if (remainingLoops > 0) {
